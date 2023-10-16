feedd98a42332b36fda3e362976aa59008059ea6
narumi <toucans-cutouts0f@icloud.com>
2023-10-16 23:13:16 +0800

    update example

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/narumiruna/go-wise/pkg/wise"
-	log "github.com/sirupsen/logrus"
 )
-
-var sourceCurrencies = []string{"GBP", "NOK", "EUR"}
 
 func main() {
 	ctx := context.Background()
+
+	sourceCurrencies := []string{"GBP", "NOK", "EUR"}
 	for _, sourceCurrency := range sourceCurrencies {
 		price, err := wise.QueryPrice(ctx, 1000, "USD", sourceCurrency)
 		if err != nil {
-			log.Errorf("query price failed: %v", err)
-			continue
+			panic(err)
 		}
 		cost, err := wise.NewCost(ctx, price)
 		if err != nil {
-			log.Errorf("failed to create cost: %v", err)
-			continue
+			panic(err)
 		}
-		log.Infof("%+v", cost)
+		fmt.Printf("%+v\n", cost)
 	}
-
 }
