877c0537e82d4c9f67c6e33718f0ee133709cffa
narumi <toucans-cutouts0f@icloud.com>
2023-10-30 15:34:17 +0800

    refactor cost struct

--- pkg/wise/cost.go
+++ pkg/wise/cost.go
@@ -18,6 +18,14 @@ type Cost struct {
 	sourceRate    float64
 	cardFeeRate   float64
 	rewardRate    float64
+
+	cardFee      float64
+	totalAmount  float64
+	wiseFeeRate  float64
+	miles        float64
+	totalFee     float64
+	totalFeeRate float64
+	milePrice    float64
 }
 
 func (c *RestClient) NewCost(ctx context.Context, source string, amount float64, target string) (*Cost, error) {
@@ -31,48 +39,33 @@ func (c *RestClient) NewCost(ctx context.Context, source string, amount float64,
 		return nil, err
 	}
 
-	return &Cost{
+	cost := &Cost{
 		Price:         price,
 		quoteCurrency: defaultQuoteCurrency,
 		sourceRate:    rate,
 		cardFeeRate:   defaultCardFeeRate,
 		rewardRate:    defaultRewardRate,
-	}, nil
+	}
+	cost.Initialize()
+	return cost, nil
 }
 
-func (c *Cost) CardFee() float64 {
-	return c.SourceAmount * c.cardFeeRate
-}
-
-func (c *Cost) TotalAmount() float64 {
-	return c.SourceAmount + c.CardFee()
-}
-
-func (c *Cost) WiseFeeRate() float64 {
-	return c.Total / c.SourceAmount
-}
-
-func (c *Cost) Miles() float64 {
-	return c.SourceAmount * c.sourceRate * c.rewardRate
-}
-
-func (c *Cost) TotalFee() float64 {
-	return c.CardFee() + c.Total
-}
-
-func (c *Cost) TotalFeeRate() float64 {
-	return c.TotalFee() / c.TotalAmount()
-}
-
-func (c *Cost) MilePrice() float64 {
-	return c.TotalFee() * c.sourceRate / c.Miles()
+func (c *Cost) Initialize() *Cost {
+	c.cardFee = c.SourceAmount * c.cardFeeRate
+	c.totalAmount = c.SourceAmount + c.cardFee
+	c.wiseFeeRate = c.Total / c.SourceAmount
+	c.miles = c.SourceAmount * c.sourceRate * c.rewardRate
+	c.totalFee = c.Total + c.cardFee
+	c.totalFeeRate = c.totalFee / c.SourceAmount
+	c.milePrice = c.totalFee * c.sourceRate / c.miles
+	return c
 }
 
 func (c *Cost) String() string {
 	s := fmt.Sprintf("Add %.2f %s", c.TargetAmount, c.TargetCurrency)
 	s += fmt.Sprintf(", pay with %.2f %s", c.SourceAmount, c.SourceCurrency)
-	s += fmt.Sprintf(", wise fee: %.2f %s (%.2f%%)", c.Total, c.SourceCurrency, c.WiseFeeRate()*100)
-	s += fmt.Sprintf(", total fee: %.2f %s (%.2f%%)", c.TotalFee(), c.SourceCurrency, c.TotalFeeRate()*100)
-	s += fmt.Sprintf(", miles: %.2f (%.2f %s/mile)", c.Miles(), c.MilePrice(), c.quoteCurrency)
+	s += fmt.Sprintf(", wise fee: %.2f %s (%.2f%%)", c.Total, c.SourceCurrency, c.wiseFeeRate*100)
+	s += fmt.Sprintf(", total fee: %.2f %s (%.2f%%)", c.totalFee, c.SourceCurrency, c.totalFeeRate*100)
+	s += fmt.Sprintf(", miles: %.2f (%.2f %s/mile)", c.miles, c.milePrice, c.quoteCurrency)
 	return s
 }
