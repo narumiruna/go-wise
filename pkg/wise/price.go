1c8cf13c6041cd0534a51ca391830b0fc5672dac
narumi <toucans-cutouts0f@icloud.com>
2023-11-11 02:41:01 +0800

    rename

--- pkg/wise/price.go
+++ pkg/wise/price.go
@@ -1,26 +1,27 @@
 package wise
 
 type Price struct {
-	PriceSetID           int               `json:"priceSetId"`
-	SourceAmount         float64           `json:"sourceAmount"`
-	TargetAmount         float64           `json:"targetAmount"`
-	PayInMethod          string            `json:"payInMethod"`
-	PayOutMethod         string            `json:"payOutMethod"`
-	SourceCurrency       string            `json:"sourceCcy"`
-	TargetCurrency       string            `json:"targetCcy"`
-	Total                float64           `json:"total"`
-	VariableFee          float64           `json:"variableFee"`
-	VariableFeePercent   float64           `json:"variableFeePercent"`
-	SwiftPayoutFlatFee   float64           `json:"swiftPayoutFlatFee"`
-	FlatFee              float64           `json:"flatFee"`
-	MidRate              float64           `json:"midRate"`
-	EcbRate              float64           `json:"ecbRate"`
-	EcbRateTimestamp     int               `json:"ecbRateTimestamp"`
-	EcbMarkupPercent     interface{}       `json:"ecbMarkupPercent"`
-	AdditionalFeeDetails *TaxDetailsByType `json:"additionalFeeDetails"`
+	PriceSetID           int64                `json:"priceSetId"`
+	SourceAmount         float64              `json:"sourceAmount"`
+	TargetAmount         float64              `json:"targetAmount"`
+	PayInMethod          string               `json:"payInMethod"`
+	PayOutMethod         string               `json:"payOutMethod"`
+	SourceCurrency       string               `json:"sourceCcy"`
+	TargetCurrency       string               `json:"targetCcy"`
+	Total                float64              `json:"total"`
+	VariableFee          float64              `json:"variableFee"`
+	VariableFeePercent   float64              `json:"variableFeePercent"`
+	SwiftPayoutFlatFee   float64              `json:"swiftPayoutFlatFee"`
+	FlatFee              float64              `json:"flatFee"`
+	MidRate              float64              `json:"midRate"`
+	EcbRate              float64              `json:"ecbRate"`
+	EcbRateTimestamp     int64                `json:"ecbRateTimestamp"`
+	EcbMarkupPercent     float64              `json:"ecbMarkupPercent"`
+	AdditionalFeeDetails AdditionalFeeDetails `json:"additionalFeeDetails"`
 }
 
-type TaxDetailsByType struct {
+type AdditionalFeeDetails struct {
 	ReceiveInr       interface{} `json:"receiveInr"`
 	TaxDetailsByType interface{} `json:"taxDetailsByType"`
+	DynamicFxFee     interface{} `json:"dynamicFxFee"`
 }
